02cd9c7dea8ffb2a359dd4fb6f201687e074df02
Duong Jonathan <djonathan91.77@gmail.com>
2023-12-11 15:22:16 +0100

    Initial version

---
+++ SeaSwiftAndSun-Simplon/SeaSwiftAndSun-Simplon/Controllers/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  SeaSwiftAndSun-Simplon
+//
+//  Created by Amandine Cousin on 05/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Liste des spots de surf"
+    }
+}
+
+
+//MARK: Handle Data source and delegate of tableview
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    // Sections
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 1:
+            return "Beach Break"
+        case 2:
+            return "Reef Break"
+        default:
+            return "Point Break"
+        }
+    }
+
+    // Rows
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 1:
+            return 1
+        default:
+            return 3
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 55
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SpotCell", for: indexPath) as! SpotCell
+        cell.setUpCell()
+        return cell
+    }
+}
